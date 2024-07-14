dd4d7d97e2973012078642d9acd12eca0fa09fe5
Mike Mehl <michaelalanmehl@gmail.com>
2024-07-14 14:03:19 -0400

    Map fix, character rectangle

--- assets/haunted-house.tsx
+++ assets/haunted-house.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="haunted-house" tilewidth="8" tileheight="8" tilecount="4332" columns="76" tilerendersize="grid">
+<tileset version="1.10" tiledversion="1.11.0" name="haunted-house" tilewidth="8" tileheight="8" tilecount="4332" columns="76" tilerendersize="grid" fillmode="preserve-aspect-fit">
  <image source="haunted_house/Tileset/HauntedHouseTileset.png" width="608" height="456"/>
 </tileset>
--- main.lua
+++ main.lua
@@ -5,6 +5,23 @@ assert(map)
 
 
 function love.load()
+  local layer = map:addCustomLayer("Sprites", 4)
+  local player
+  for _, obj in pairs(map.objects) do
+    if obj.name == "PlayerStart" then
+      player = obj
+      break
+    end
+  end
+
+  layer.player = {
+    x = player.x,
+    y = player.y
+  }
+
+  layer.draw = function(self)
+    love.graphics.rectangle("fill", self.player.x, self.player.y, 8, 8)
+  end
 end
 
 function love.update(dt)
@@ -12,5 +29,5 @@ function love.update(dt)
 end
 
 function love.draw()
-  map:draw()
+  map:draw(0, 0, 4, 4)
 end
