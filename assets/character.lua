788bec86d8139c317aebdaaf03039903743d8f1b
Mike Mehl <michaelalanmehl@gmail.com>
2024-07-18 14:34:35 -0400

    Add character animation to tiled.

---
+++ assets/character.lua
@@ -0,0 +1,116 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  name = "character",
+  class = "",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 4,
+  image = "characters/HumanTownsfolkWalk.png",
+  imagewidth = 128,
+  imageheight = 128,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 225,
+  tiles = {
+    {
+      id = 3,
+      animation = {
+        {
+          tileid = 0,
+          duration = 100
+        },
+        {
+          tileid = 1,
+          duration = 100
+        },
+        {
+          tileid = 2,
+          duration = 100
+        },
+        {
+          tileid = 3,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 7,
+      animation = {
+        {
+          tileid = 4,
+          duration = 100
+        },
+        {
+          tileid = 5,
+          duration = 100
+        },
+        {
+          tileid = 6,
+          duration = 100
+        },
+        {
+          tileid = 7,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 11,
+      animation = {
+        {
+          tileid = 8,
+          duration = 100
+        },
+        {
+          tileid = 9,
+          duration = 100
+        },
+        {
+          tileid = 10,
+          duration = 100
+        },
+        {
+          tileid = 11,
+          duration = 100
+        }
+      }
+    },
+    {
+      id = 15,
+      animation = {
+        {
+          tileid = 12,
+          duration = 100
+        },
+        {
+          tileid = 13,
+          duration = 100
+        },
+        {
+          tileid = 14,
+          duration = 100
+        },
+        {
+          tileid = 15,
+          duration = 100
+        }
+      }
+    }
+  }
+}
--- assets/character.tsx
+++ assets/character.tsx
@@ -1,4 +1,37 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="character" tilewidth="8" tileheight="8" tilecount="1024" columns="64">
- <image source="characters/HumanTownsfolkIdle.png" width="512" height="128"/>
+<tileset version="1.10" tiledversion="1.11.0" name="character" tilewidth="32" tileheight="32" tilecount="225" columns="4">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="characters/HumanTownsfolkWalk.png" width="128" height="128"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
