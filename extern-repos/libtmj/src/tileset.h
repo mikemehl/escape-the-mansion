8fb581f107aea91541a30fd865fa0e6403d869c2
Mike Mehl <michaelalanmehl@gmail.com>
2024-08-12 11:34:35 -0400

    Remove the extra cruft.

--- extern-repos/libtmj/src/tileset.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBTMJ_TILESET
-#define LIBTMJ_TILESET
-
-#include "tmj.h"
-
-int unpack_tileset(json_t* tileset, Tileset* ret);
-void tilesets_free(Tileset* tilesets, size_t tileset_count);
-
-#endif
